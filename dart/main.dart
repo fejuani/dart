525c8e51c2ff95553341b5b0edc5607ee60bbf1c
fejuani <103078863+fejuani@users.noreply.github.com>
2023-02-06 15:31:16 -0300

    Add files via upload

---
+++ dart/main.dart
@@ -0,0 +1,34 @@
+void main() {
+  var pi = 3.14159;
+  var r = 2;
+  var a = pi * (r * r);
+
+  print('exercicio 1:  $a');
+
+  var chapa = 25;
+  var horas = 100;
+  var shora = 5.50;
+  var salario = horas * shora;
+
+  print('exercicio 2: funcionario com numero de chama $chapa recebe $salario reais');
+
+  var nome = "João";
+  var salario2 = 500;
+  var comissao = 1230.30 * 0.15;
+  var salariom = salario2 + comissao;
+
+  print('exercicio 3: $nome recebeu $salariom neste mes por conta dos $comissao de suas vendas');
+
+  var q = 50;
+  var t = 12;
+  var delta = 100;
+  var u = t - delta - q;
+
+  print(u);
+
+  var trabalho = 12000;
+  var q1 = 23000;
+  var n = trabalho / q1 * 100;
+
+  print('exercicio 5: a capacidade percentual de trabalho é de $n%');
+}
